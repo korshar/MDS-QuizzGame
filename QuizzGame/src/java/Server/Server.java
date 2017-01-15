cbc47863f723b6d8b2225e8afd61931c0b6badbc
JonathanMaga <jonathanmagalhaes18@gmail.com>
2017-01-15 12:12:50 +0000

    Branch Server

--- QuizzGame/src/java/Server/Server.java
+++ QuizzGame/src/java/Server/Server.java
@@ -5,10 +5,51 @@
  */
 package Server;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
 /**
  *
  * @author Andre Pinho
  */
+@Singleton
+@LocalBean
 public class Server {
+    List<String> utilizadores;
+
+    public Server() {
+        utilizadores = new ArrayList<>();
+    }
     
+    public String getUser(String nome){
+        for(String u : utilizadores){
+            if(u.equals(nome))
+                return u;
+        }
+        return null;
+    }
+    
+    public void addUser(String nome){
+        utilizadores.add(nome);
+    }
+    
+    public void clearList(){
+        utilizadores.clear();
+    }
+    
+    public void removeUser(String nome){
+        utilizadores.stream().filter((u) -> (u.equals(nome))).forEach((u) -> {
+            utilizadores.remove(u);
+        });
+    }
+    
+    public boolean aprovUserName(String nome){
+       for(String s : utilizadores){
+           if(s.equals(nome))
+               return false;
+       }
+       return true;
+    }
 }
